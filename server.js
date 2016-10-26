25e19ef734468866b35dfd78c636f9e9d97538aa
Allan Martine <alm050@ucsd.edu>
2016-10-25 21:39:05 -0700

    getting rid of all the junk

--- server.js
+++ server.js
@@ -1,55 +1,22 @@
-
-var express = require('express');
+var express = require('express')
 var app = express();
 
-//set the port of our application
-//proces.env.PORT lets the port 
+//set the port of our application 
+//proces.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
-//set view engine to ejs
+//set the view engine to ejs
 app.set('view engine', 'ejs');
-
-var router = express.Router();
-var path = __dirname + '/views/';
 
 //make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
 
-router.use(function (req, res, next){
-	console.log("/" + req.method);
-	next();
-});
-
-router.get("/", function(req, res){
-	res.sendFile(path + "index.ejs");
-});
-
-router.get("/eventpage", function(req,res){
-	res.sendFile(path + "eventpage.html");
-});
-
-router.get("/hostpage", function(req, res){
-	res.sendFile(path + "hostpage.html");
-});
-
-router.get("/profilepage", function(req, res){
-	res.sendFile(path + "profilepage.html");
-});
-
-router.get("/addeventpage", function(req, res){
-	res.sendFile(path + "addeventpage.html");
-});
-
-router.get("/reportpage", function(req, res){
-	res.sendFile(path + "reportpage.html");
-}); 
-
-app.use("/", router);
-
-app.use("*", function(req, res){
-	res.sendFile(path + "404.html");
+//set the home page route
+app.get('/', function(req, res){
+   //ejs render automatically looks in the views folder
+   res.render('index');
 });
 
 app.listen(port, function(){
-	console.log('Our app is running on http://localhost:' + port);
+   console.log('Our app is running on http://localhost:' + port);
 });
