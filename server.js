b91c6edbdaa84467b51389e6c3ad3ebfebd13e1d
sjpl <slaub@ucsd.edu>
2016-10-27 04:10:39 -0700

    Modified server.js to test support for additional pages. Added new pages.

--- server.js
+++ server.js
@@ -1,47 +1,27 @@
-var express = require('express')
+var express = require('express');
 var app = express();
 
-//set the port of our application 
-//proces.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
+app.set('port', (process.env.PORT || 8080));
 
-//set the view engine to ejs
-app.set('view engine', 'ejs');
-
-//make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
 
-//set the home page route
-app.get('/', function(req, res){
-   //ejs render automatically looks in the views folder
-   res.render('index');
-});
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
-app.get('/addeventpage', function(req, res){
-	//ejs render automatically looks in the views folder
-	res.render(addeventpage);
-});
 
-app.get('/eventpage', function(req, res){
-	//ejs render automatically looks in the views folder
-	res.render(eventpage);
-});
+//Create link to other pages in the views directory.
+app.get('/', function(request, response) 
+	{response.render('index');});
+app.get('/hostlist', function(request, response) 
+	{response.render('hostlist');});
+app.get('/profilepage', function(request, response) 
+	{response.render('profilepage');});
+app.get('/profilepage-notification', function(request, response) 
+	{response.render('profilepage-notification');});
+app.get('/filterpage', function(request, response) 
+	{response.render('filterpage');});
 
-app.get('/hostpage', function(req, res){
-	//ejs render automatically looks in the views folder
-	res.render(hostpage);
-});
-
-app.get('/reportpage', function(req, res){
-	//ejs render automatically looks in the views folder
-	res.render(reportpage);
-});
-
-app.get('/settingspage', function(req, res){
-	//ejs render automatically looks in the views folder
-	res.render(settingspage);
-});
-
-app.listen(port, function(){
-   console.log('Our app is running on http://localhost:' + port);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
