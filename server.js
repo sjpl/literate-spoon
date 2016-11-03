84208e99d6c06176ae2333346500d0a7c5f1206b
sjpl <slaub@ucsd.edu>
2016-11-03 14:36:42 -0700

    Updated .ejs to .handlebars.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var handlebars = require('handlebars');
+var handlebars = require('express-handlebars')
 var app = express();
 
 //set the port of our application 
@@ -11,46 +11,36 @@ app.use(express.static(__dirname + '/public'));
 
 //views is directory for all template files
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+
+
 
 //create link to other pages in the views directory
 app.get('/', function (request, response) {
-    response.render('index');
-});
+    response.render('index');});
 app.get('/indexin', function(request, response){
-	response.render('indexin');
-});
+	response.render('indexin');});
 app.get('/hostlist', function (request, response){
-	response.render('hostlist');
-});
+	response.render('hostlist');});
 app.get('/hostpage', function (request, response) {
-    response.render('hostpage');
-});
+    response.render('hostpage');});
 app.get('/hostpage-pastevents', function (request, response) {
-    response.render('hostpage-pastevents');
-});
+    response.render('hostpage-pastevents');});
 app.get('/profilepage', function (request, response) {
-    response.render('profilepage');
-});
+    response.render('profilepage');});
 app.get('/profilepage-notification', function (request, response) {
-    response.render('profilepage-notification');
-});
+    response.render('profilepage-notification');});
 app.get('/filterpage', function (request, response) {
-    response.render('filterpage');
-});
-
+    response.render('filterpage');});
 app.get('/addeventpage', function (request, response) {
-    response.render('addeventpage');
-});
+    response.render('addeventpage');});
 app.get('/eventpage', function (request, response) {
-    response.render('eventpage');
-});
+    response.render('eventpage');});
 app.get('/aboutpage', function (request, response) {
-    response.render('aboutpage');
-});
+    response.render('aboutpage');});
 app.get('/discusspage', function (request, response) {
-    response.render('discusspage');
-});
+    response.render('discusspage');});
 
 app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
